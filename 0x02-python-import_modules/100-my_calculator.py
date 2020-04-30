68147e6f9892a95830a39e7012811b4f48940184
Antonio Estela <antonioestela73@gmail.com>
2020-04-30 20:04:53 +0000

    Fixed task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
     if len(args) is not 3:
         print("Usage: ./100-my_calculator.py <a> <op> <b>")
         exit(1)
-    num1 = int(args[0])
-    num2 = int(args[2])
+    n1 = int(args[0])
+    n2 = int(args[2])
     op = args[1]
     if op not in operators:
         print("Unknown op. Available ops: +, -, * and /")
         exit(1)
     elif op == '+':
-        print("{:d} {} {:d} = {:d}".format(num1, op, num2, calc.add(num1, num2)))
+        print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.add(n1, n2)))
     elif op == '-':
-        print("{:d} {} {:d} = {:d}".format(num1, op, num2, calc.sub(num1, num2)))
+        print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.sub(n1, n2)))
     elif op == '*':
-        print("{:d} {} {:d} = {:d}".format(num1, op, num2, calc.mul(num1, num2)))
+        print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.mul(n1, n2)))
     elif op == '/':
-        print("{:d} {} {:d} = {:d}".format(num1, op, num2, calc.div(num1, num2)))
+        print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.div(n1, n2)))
