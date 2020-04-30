ed67ad7b9f17bd6327e06cd4c05d9cc99c80e82d
Antonio Estela <antonioestela73@gmail.com>
2020-04-30 17:40:32 +0000

    Fixed task 2 (or i think so)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     a = 1
     if len(args) == 0:
         print("{:d} arguments.".format(len(args)))
+    elif len(args) == 1:
+        print("{:d} argument:".format(len(args)))
+        print("{:d}: {}".format(a, args[0]))
     else:
         print("{:d} arguments:".format(len(args)))
         for i in args:
