a3050ae97187f32ce98495694c10b08467b59b9a
Antonio Estela <antonioestela73@gmail.com>
2020-04-30 20:14:12 +0000

    Fixed task 100 (or i hope so)

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import calculator_1 as calc
     import sys
-    operators = ['+', '-', '*', '/']
     args = sys.argv[1:]
     if len(args) is not 3:
         print("Usage: ./100-my_calculator.py <a> <op> <b>")
@@ -10,10 +9,7 @@ if __name__ == "__main__":
     n1 = int(args[0])
     n2 = int(args[2])
     op = args[1]
-    if op not in operators:
-        print("Unknown op. Available ops: +, -, * and /")
-        exit(1)
-    elif op == '+':
+    if op == '+':
         print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.add(n1, n2)))
     elif op == '-':
         print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.sub(n1, n2)))
@@ -21,3 +17,6 @@ if __name__ == "__main__":
         print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.mul(n1, n2)))
     elif op == '/':
         print("{:d} {} {:d} = {:d}".format(n1, op, n2, calc.div(n1, n2)))
+    else:
+        print("Unknown op. Available ops: +, -, * and /")
+        exit(1)
