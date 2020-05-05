3f27363aa3d980364e5b13fe845cf2e5aaecfc2f
Antonio Estela <antonioestela73@gmail.com>
2020-05-05 21:36:36 +0000

    Fixed task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,5 @@ def max_integer(my_list=[]):
     max_int = 0
     if len(my_list) == 0:
         return None
-
-    for i in my_list:
-        if i > max_int:
-            max_int = i
-    return max_int
+    my_list.sort()
+    return my_list[-1]
