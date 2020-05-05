4c6a0b5ae7fd693c0285445d54440bdda12a3f77
Antonio Estela <antonioestela73@gmail.com>
2020-05-05 21:23:47 +0000

    Fixed pep8 code style

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,6 @@ def max_integer(my_list=[]):
         return None
 
     for i in my_list:
-       if i > max_int:
-           max_int = i
+        if i > max_int:
+            max_int = i
     return max_int
