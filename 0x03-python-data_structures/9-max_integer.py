43acf7626478383403ce3511e5d5707cbb1ef21f
Antonio Estela <antonioestela73@gmail.com>
2020-05-05 21:22:50 +0000

    Added task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/puthon3
+def max_integer(my_list=[]):
+    max_int = 0
+    if len(my_list) == 0:
+        return None
+
+    for i in my_list:
+       if i > max_int:
+           max_int = i
+    return max_int
