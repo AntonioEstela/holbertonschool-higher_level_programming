a20d5e5a30524463a69cbc34b7374bbb73af2385
Antonio Estela <antonioestela73@gmail.com>
2020-05-05 21:01:17 +0000

    Fixed task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,5 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0, 0)
     if len(tuple_b) < 2:
         tuple_b = tuple_b + (0, 0)
-    tuple_sum = tuple(map(sum, zip(tuple_a, tuple_b)))
+    tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return tuple_sum
