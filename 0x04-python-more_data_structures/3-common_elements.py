8877bb37d98daed9ba83bbe0524b2ee7cb1b90cd
Antonio Estela <antonioestela73@gmail.com>
2020-05-08 00:32:25 +0000

    Optimized task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new = []
-    for element in set_1:
-        if element in set_2:
-            new.append(element)
-    return new
+    return set_1 & set_2
