9c8af938f478675b414d53f6b6996985884e2a5f
Antonio Estela <antonioestela73@gmail.com>
2020-05-18 18:46:39 +0000

    Fixed task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             cnt += 1
-        except:
+        except (TypeError, ValueError):
             pass
     print("")
     return cnt
