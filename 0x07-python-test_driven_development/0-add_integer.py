9596878b3571f5538f81db3f2521fb0898a7895e
Antonio Estela <antonioestela73@gmail.com>
2020-05-23 12:04:12 -0500

    Fixed task 0 and added new test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,22 +11,10 @@ def add_integer(a, b=98):
        Parameters: a and b must be and integers or floats
        otherwhise a TypeError will raised."""
 
-    if type(a) is not int and type(a) is not float:
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
 
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
-    if type(a) is float:
-        a = int(a)
-
-    if type(b) is float:
-        b = int(b)
-
-    if a is None:
-        a = 0
-
-    if b is None:
-        b = 0
-
-    return a + b
+    return int(a + b)
