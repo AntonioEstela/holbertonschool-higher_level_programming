ea330fd38bf6aaf4546fb47c57cb2480de4702d3
Antonio Estela <antonioestela73@gmail.com>
2020-05-25 18:27:51 -0500

    Fixed task 1, or i think so

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Instantation"""
 
-        self.__height = height
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
