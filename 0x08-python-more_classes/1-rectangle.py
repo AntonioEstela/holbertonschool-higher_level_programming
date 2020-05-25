ae256046cdaf369b0ad89f90fc0bd9b9a62f7060
Antonio Estela <antonioestela73@gmail.com>
2020-05-25 18:26:47 -0500

    Fixed task 1, or i think so

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,13 +20,13 @@ class Rectangle:
     def width(self, value):
         """Setting a new value for width"""
 
-        self.__width = value
-
         if type(value) is not int:
             raise TypeError("width must be an integer")
 
         if value < 0:
             raise ValueError("width must be >= 0")
+
+        self.__width = value
 
     @property
     def height(self):
@@ -37,10 +37,10 @@ class Rectangle:
     def height(self, value):
         """Setting a new value for height"""
 
-        self.__height = value
-
         if type(value) is not int:
             raise TypeError("height must be an integer")
 
         if value < 0:
             raise ValueError("height must be >= 0")
+
+        self.__height = value
