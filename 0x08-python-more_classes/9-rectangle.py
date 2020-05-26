12678d2085605e8a862a39e8a6b8972a48bcff03
Antonio Estela <antonioestela73@gmail.com>
2020-05-25 19:31:50 -0500

    Fixed some things :D, or i think so

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Instantation"""
 
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __repr__(self):
