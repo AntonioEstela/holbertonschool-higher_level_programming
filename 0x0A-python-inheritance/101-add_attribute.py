9baef1bfed78928f7e74a2a14a4236a2e7bebbb7
Antonio Estela <antonioestela73@gmail.com>
2020-06-02 18:50:53 -0500

    Fixed task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module for add_atribute method"""
+
+
 def add_attribute(obj, att, val):
-    if not hasattr(obj, '__dict__'):
+    """"Module for add_atribute method"""
+    if hasattr(obj, '__dict__') is True:
+        setattr(obj, att, val)
+    else:
         raise TypeError("can't add new attribute")
-    setattr(obj, att, val)
