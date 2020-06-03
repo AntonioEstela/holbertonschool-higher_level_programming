fd4f03125b4a27cc8960720a227a1adcad512a29
Antonio Estela <antonioestela73@gmail.com>
2020-06-03 18:16:55 -0500

    Fixed task 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -13,4 +13,10 @@ class Student:
 
     def to_json(self, attrs=None):
         """to json"""
+        if type(attrs) is list and all(isinstance(i, str) for i in attrs):
+            nd = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    nd[k] = v
+            return nd
         return self.__dict__
