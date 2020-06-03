c924e68dd2ad3d8e23cf86c57ad801bd330805a8
Antonio Estela <antonioestela73@gmail.com>
2020-06-03 18:19:48 -0500

    Added task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" student class"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json"""
+        if type(attrs) is list and all(isinstance(i, str) for i in attrs):
+            nd = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    nd[k] = v
+            return nd
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace"""
+        for k, v in json.items():
+            self.__dict__[k] = v
