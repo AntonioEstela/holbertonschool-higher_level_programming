4be22eede3f67fbcd265bff57fa1848815d9edda
Antonio Estela <antonioestela73@gmail.com>
2020-06-09 15:26:21 -0500

    Fixed attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,8 +30,8 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """update"""
