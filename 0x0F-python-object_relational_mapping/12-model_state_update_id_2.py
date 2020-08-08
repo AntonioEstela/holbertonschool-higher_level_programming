4c6f277bd61135fb45b39fadae2df2fffa994e7f
Antonio Estela <antonioestela73@gmail.com>
2020-08-08 10:21:36 -0500

    Added task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Some text """
+
+import sys
+import sqlalchemy as db
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    """ Conecting to a database """
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                              .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                              pool_pre_ping=True)
+
+    metadata = db.MetaData()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    name = session.query(State).filter(State.id == 2).first()
+
+    name.name = 'New Mexico'
+
+    session.commit()
+
+    session.close()
