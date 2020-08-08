d4e2da7cd2f5fcdd3a9d843962bf3f8a5b17169e
Antonio Estela <antonioestela73@gmail.com>
2020-08-08 10:20:02 -0500

    Added task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa: """
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    database = MySQLdb.connect('localhost', username, password, database_name)
+
+    cursor = database.cursor()
+
+    cursor.execute("""SELECT cities.name FROM cities
+                      INNER JOIN states ON cities.state_id = states.id
+                      WHERE states.name = %(state_name)s
+                      ORDER BY cities.id ASC;""", {'state_name': sys.argv[4]})
+
+    states = cursor.fetchall()
+
+    for n_row in range(len(states)):
+        if n_row < len(states) - 1:
+            print(states[n_row][0], end=", ")
+        else:
+            print(states[n_row][0])
+
+    database.close()
