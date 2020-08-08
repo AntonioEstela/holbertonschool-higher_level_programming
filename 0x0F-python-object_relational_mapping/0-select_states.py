9a546151acdd138c911b99295294206ada7712d2
Antonio Estela <antonioestela73@gmail.com>
2020-08-08 10:19:02 -0500

    Added task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa: """
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    database = MySQLdb.connect('localhost', username, password, database_name)
+
+    cursor = database.cursor()
+
+    cursor.execute("SELECT * from states ORDER BY states.id ASC;")
+
+    states = cursor.fetchall()
+
+    for row in states:
+        print(row)
+
+    database.close()
