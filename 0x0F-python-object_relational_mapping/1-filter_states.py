9f9d63e80de20648739cbdd8d6b89254e8f79615
Antonio Estela <antonioestela73@gmail.com>
2020-08-08 10:27:19 -0500

    Fixed task 1 (or i think so)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     cursor = database.cursor()
 
-    cursor.execute("""SELECT id, name FROM states WHERE name LIKE 'N%'
+    cursor.execute("""SELECT id, name FROM states WHERE name LIKE BINARY 'N%'
                       ORDER BY states.id;""")
 
     states = cursor.fetchall()
