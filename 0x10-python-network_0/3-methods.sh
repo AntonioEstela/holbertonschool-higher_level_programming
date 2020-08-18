8e7e1c8db6493f89242afa07015b208216b6f1bd
Antonio Estela <antonioestela73@gmail.com>
2020-08-18 13:51:51 -0500

    Fixed task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX "$1" | grep -e "Allow" | cut -d":" -f2 | sed -e 's/^ //'
+curl -sIX OPTIONS "$1" | grep -e "Allow" | cut -d":" -f2 | sed -e 's/^ //'
