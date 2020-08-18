5380fe6479c589d6dc06844c6d15ea91258a8f24
Antonio Estela <antonioestela73@gmail.com>
2020-08-18 15:31:14 -0500

    Fixed task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
-curl -sX POST -d "email=hr@holbertonschool.com&subjet=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -s -d "email=hr@holbertonschool.com&subjet=I%20will%20always%20be%20here%20for%20PLD" "$1"
