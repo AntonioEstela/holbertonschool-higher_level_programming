34dc446ffeeebc953dc53f8e1b7ff67b19a0a348
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 17:56:38 -0500

    Fixed task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,13 +14,16 @@ if __name__ == "__main__":
     else:
         letter = sys.argv[1]
 
-    req = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
+    url = 'http://0.0.0.0:5000/search_user'
+    value = {'q': letter}
+
+    req = requests.post(url, value)
 
     try:
         reqjson = req.json()
         if reqjson:
-            print("[{}] {}".format(reqjson["id"], reqjson["name"]))
+            print('[{}] {}'.format(reqjson['id'], reqjson['name']))
         else:
-            print("No result")
+            print('No result')
     except Exception:
-        print("Not a valid JSON")
+        print('Not a valid JSON')
