44a8ff08d05c903ff433af439658bff324399eca
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 18:49:08 -0500

    Added advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that lists 10 commits (from the most recent to oldest) \
+from a given repository of a given user in github"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        owner, repository)
+
+    req = requests.get(url)
+    reqjson = req.json()
+
+    for commit in range(10):
+        print('{}: {}'.format(reqjson[commit].get(
+            'sha'), reqjson[commit]['commit']['author']['name']))
