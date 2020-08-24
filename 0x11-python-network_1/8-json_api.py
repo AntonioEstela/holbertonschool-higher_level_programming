29be7b88463e20a903402ec97cf654114a2bd689
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 17:36:38 -0500

    Fixed task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Write a Python script that takes in a letter and sends a POST\
- request to http://0.0.0.0:5000/search_user\
- with the letter as a parameter."""
+ request to http://0.0.0.0:5000/search_user \
+with the letter as a parameter."""
 
 import sys
 import requests
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     try:
         letter = sys.argv[1]
-    except IndexError:
+    except Exception:
         letter = ""
 
     url = 'http://0.0.0.0:5000/search_user'
