78153f77caff69afb40e43075e19ea27f090f815
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 14:52:16 -0500

    Fixed pep8 code style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,8 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     content = response.read()
     utf8_content = content.decode('utf-8')
-    
+
 print('Body response:')
 print('\t- type: {}'.format(type(content)))
 print('\t- content: {}'.format(content))
 print('\t- utf8 content: {}'.format(utf8_content))
-
