a03f21bc8871f04d4456f0ebd005069775fb0356
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 17:50:49 -0500

    Fixed task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a letter and sends a POST\
- request to http://0.0.0.0:5000/search_user \
-with the letter as a parameter."""
+"""Script that takes URL and a email, sends POST reques tto the URL"""
 
-import sys
 import requests
+from sys import argv
 
 if __name__ == "__main__":
-
-    if len(sys.argv) == 1:
-        letter = ""
-
+    if len(argv) == 1:
+        q = ""
     else:
-        letter = sys.argv[1]
+        q = argv[1]
 
-    req = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
+    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
 
     try:
-        reqjson = req.json()
-        if reqjson:
-            print("[{}] {}".format(reqjson["id"], reqjson["name"]))
+        data = res.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
         else:
             print("No result")
-    except Exception:
+    except:
         print("Not a valid JSON")
