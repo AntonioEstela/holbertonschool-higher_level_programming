55b54f33a39686d2c44e9f3bf8a12c4565ed9e31
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 19:44:04 -0500

    Trying to fix advanced task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,23 +2,19 @@
 """Python script that lists 10 commits (from the most recent to oldest) \
 from a given repository of a given user in github"""
 
-import sys
-import requests
-
 if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
 
     try:
-        repository = sys.argv[1]
-        owner = sys.argv[2]
-
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(
-            owner, repository)
-
-        req = requests.get(url)
-        reqjson = req.json()
-
-        for commit in reqjson[:10]:
-            print('{}: {}'.format(commit.get('sha'), commit.get(
-                'commit').get('author').get('name')))
-    except Exception as error:
-        print(error)
+        req = get(url)
+        j = req.json()
+        for commit in j[:10]:
+            print('{}: {}'.format(commit.get('sha'),
+                                  commit.get('commit')
+                                  .get('author')
+                                  .get('name')))
+    except IndexError as err:
+        print(err)
