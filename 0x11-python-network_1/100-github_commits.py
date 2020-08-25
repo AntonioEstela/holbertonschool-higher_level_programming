915cf9b850558b4c82e5415a4a6c740f6df04530
Antonio Estela <antonioestela73@gmail.com>
2020-08-24 19:53:51 -0500

    Fixed task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,20 +7,21 @@ Get the 10 most recent commits from the repository specified
 import sys
 import requests
 if __name__ == "__main__":
-    repository = sys.argv[1]
-    owner = sys.argv[2]
-
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(
-        owner, repository)
-
-    req = requests.get(url)
-
     try:
+        repository = sys.argv[1]
+        owner = sys.argv[2]
+
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(
+            owner, repository)
+
+        req = requests.get(url)
+
         reqjson = req.json()
 
         for commit in range(10):
-            print('{}: {}'.format(reqjson[commit].get(
-                'sha'), reqjson[commit].get('commit').get('author').get('name')))
+            print('{}: {}'.format(reqjson[commit].get('sha'),
+                                  reqjson[commit].get('commit')
+                                  .get('author').get('name')))
 
     except Exception as error:
         print(error)
