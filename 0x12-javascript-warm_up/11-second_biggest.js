6e720e3f74d6fb81948769de3ee32f1ccd234200
Antonio Estela <antonioestela73@gmail.com>
2020-09-15 17:21:15 -0500

    Fixed task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const numbers = process.argv.slice(2);
 
-if (numbers.length === 0 || numbers.length === 1) {
-  console.log(0);
+if (numbers.length < 2) {
+  console.log('0');
 } else {
   numbers.sort().reverse();
   console.log(numbers[1]);
