979cc9a8af1b0493bb03ab4b93929e39b1244a7f
Antonio Estela <antonioestela73@gmail.com>
2020-09-15 15:41:46 -0500

    Added task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const times = parseInt(process.argv[2]);
+
+if (!isNaN(times)) {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
