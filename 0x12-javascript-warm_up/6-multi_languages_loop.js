ba751ee260f71d14f78b3271bb340a333d263afe
Antonio Estela <antonioestela73@gmail.com>
2020-09-15 15:24:28 -0500

    Fixed code style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,5 @@
 const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let index = 0; index < languages.length; index++) {
-  console.log(languages[index])
-
+  console.log(languages[index]);
 }
