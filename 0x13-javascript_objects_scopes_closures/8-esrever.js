fa9706b651debc6aa45550bb19c3c5e9d9beba93
Antonio Estela <antonioestela73@gmail.com>
2020-09-21 13:40:24 -0500

    Changed var for let

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  var newList = [];
+  let newList = [];
   for (let index = list.length - 1; index >= 0; index--) {
     newList.push(list[index]);
   }
