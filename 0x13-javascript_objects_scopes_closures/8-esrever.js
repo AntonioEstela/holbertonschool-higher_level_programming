4a26f6d541ca201136269207059e9f3008bc1edb
Antonio Estela <antonioestela73@gmail.com>
2020-09-21 13:41:09 -0500

    Fixed task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let newList = [];
+  const newList = [];
   for (let index = list.length - 1; index >= 0; index--) {
     newList.push(list[index]);
   }
