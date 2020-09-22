6a8cd8ca6fdd8b6a315ba36cbe7b7c8229d96d9c
Antonio Estela <antonioestela73@gmail.com>
2020-09-22 14:07:59 -0500

    Fixed output whitespace

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (!error && response && body) {
-    console.log('code: ', response && response.statusCode);
+    console.log('code:', response && response.statusCode);
   }
 });
