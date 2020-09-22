ca30de09de01c7f92e695e6d032f1c5aba70a05a
Antonio Estela <antonioestela73@gmail.com>
2020-09-22 15:47:12 -0500

    Fixed task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
 
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
-    const films = data.results;
     let count = 0;
 
-    for (const film of films) {
-      const characters = film.characters;
-      if (characters.includes(character)) {
-        count++;
-      }
+    for (const film of data.results) {
+      for (const char of film.characters)
+        if (char.endsWith('18/')) {
+          count++;
+        }
     }
     console.log(count);
   }
